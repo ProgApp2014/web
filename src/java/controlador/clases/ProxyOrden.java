6825d12ee1f462a25cd42a1375bc5a75eef48b32
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2014-09-21 14:48:00 -0300

    ProxyOrden con conversion

--- src/java/controlador/clases/ProxyOrden.java
+++ src/java/controlador/clases/ProxyOrden.java
@@ -14,6 +14,8 @@ import controlador.datatypes.DataCategoriaWS;
 import controlador.datatypes.DataClienteWS;
 import controlador.datatypes.DataEspecificacionProductoWS;
 import controlador.datatypes.DataOrdenCompraWS;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -21,106 +23,136 @@ import java.util.List;
  * @author rodro
  */
 public class ProxyOrden {
-    
+
     private final Integer idOrdenesControlador;
-    
+
     public ProxyOrden() {
         idOrdenesControlador = Fabrica.getInstance().getControladorOrdenes(null).getId();
     }
-    
+
     private static ProxyOrden instance = null;
-    
+
     public static ProxyOrden getInstance() {
-        
+
         if (ProxyOrden.instance == null) {
-            
+
             ProxyOrden.instance = new ProxyOrden();
-            
+
         }
         return ProxyOrden.instance;
     }
-    public String retNombre(){
+
+    public String retNombre() {
         return "sapeeee rodro";
     }
+
     public List<DataClienteWS> listarClientes() {
-        List<DataCliente>  l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarClientes();
-        
-        return null;
-        
+        List<DataCliente> l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarClientes();
+        List<DataClienteWS> ret = new ArrayList();
+        Iterator it = l.iterator();
+        while (it.hasNext()) {
+            ret.add(ConvertidorFrontEnd.toCliente((DataCliente) it.next()));
+        }
+        return ret;
+
     }
-    
+
     public void elegirCliente(String nickname) {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirCategoria(nickname);
     }
-    
+
     public List<DataCategoriaWS> listarCategorias() {
         List<DataCategoria> l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarCategorias();
-        return null;
-        
+        List<DataCategoriaWS> ret = new ArrayList();
+        Iterator it = l.iterator();
+        while (it.hasNext()) {
+            ret.add(ConvertidorFrontEnd.toCategoria((DataCategoria) it.next()));
+        }
+        return ret;
+
     }
-    
+
     public void elegirCategoria(String categoria) {
-         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirCategoria(categoria);
-        
+        Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirCategoria(categoria);
+
     }
-    
+
     public List<DataEspecificacionProductoWS> listarEspecificacionProductos() {
         List<DataEspecificacionProducto> l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarEspecificacionProductos();
-        
-        return null;
-        
+
+        List<DataEspecificacionProductoWS> ret = new ArrayList();
+        Iterator it = l.iterator();
+        while (it.hasNext()) {
+            ret.add(ConvertidorFrontEnd.toEspecificacionProducto((DataEspecificacionProducto) it.next()));
+        }
+        return ret;
+
     }
-    
+
     public void elegirEspecificacionProducto(String nroRef) {
-         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirEspecificacionProducto(nroRef);
+        Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirEspecificacionProducto(nroRef);
     }
- 
+
     public List<DataEspecificacionProductoWS> listarProductosEnOrden() {
-        return null;
-        
+
+        List<DataEspecificacionProducto> l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarProductosEnOrden();
+       
+        List<DataEspecificacionProductoWS> ret = new ArrayList();
+        Iterator it = l.iterator();
+        while (it.hasNext()) {
+            ret.add(ConvertidorFrontEnd.toEspecificacionProducto((DataEspecificacionProducto) it.next()));
+        }
+        return ret;
+
     }
-    
+
     public void elegirProducto(Integer id) {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirProducto(id);
     }
-    
+
     public void elegirCantidadProducto(Integer cantidad) {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirCantidadProducto(cantidad);
     }
-    
+
     public void generarItemOrden() {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).generarItemOrden();
     }
-    
+
     public void guardarOrden(DataOrdenCompraWS dataOrden) {
-        
-        Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).guardarOrden(null);
+
+        Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).guardarOrden(ConvertidorBackend.toOrden(dataOrden));
     }
-    
+
     public List<DataOrdenCompraWS> listarOrdenes() {
-        
+
         List<DataOrdenCompra> l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).listarOrdenes();
-        return null;
         
+        List<DataOrdenCompraWS> ret = new ArrayList();
+        Iterator it = l.iterator();
+        while (it.hasNext()) {
+            ret.add(ConvertidorFrontEnd.toOrden((DataOrdenCompra) it.next()));
+        }
+        return ret;
+
     }
-    
+
     public void elegirOrden(Integer nroOrden) {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).elegirOrden(nroOrden);
-        
+
     }
-    
+
     public void borrarOrdenCompra() {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).borrarOrdenCompra();
     }
-    
+
     public DataOrdenCompraWS mostrarDatosOrden() {
         DataOrdenCompra l = Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).mostrarDatosOrden();
-        return null;
-        
+        return ConvertidorFrontEnd.toOrden(l);
+
     }
-    
+
     public void removerEspecificacionProducto(String ref) {
         Fabrica.getInstance().getControladorOrdenes(idOrdenesControlador).removerEspecificacionProducto(ref);
     }
-    
+
 }
